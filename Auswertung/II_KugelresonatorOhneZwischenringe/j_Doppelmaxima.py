42f158ba4aa81074540d1580caaf74d151361e30
letTheWhalesFly <41802557+letTheWhalesFly@users.noreply.github.com>
2023-08-06 18:56:00 +0200

    bissl auswertung

---
+++ Auswertung/II_KugelresonatorOhneZwischenringe/j_Doppelmaxima.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("..")
+from header.APbrainLite import *
+
+
+# ---DATEN---
+
+
+# TODO
+# Spektrum aus 2.h) plotten
---
+++ Auswertung/I_Rohrresonator/f_Lorentzfit.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("..")
+from header.APbrainLite import *
+
+
+# ---DATEN---
+
+
+# TODO
+# Frequenzspektren aus 1.c) und 1.d) plotten
+# NUR FÜR DATEN AUS 1.d): sieben+ Peaks auswählen und für die Messpunkte um jeden dieser Peak einen Lorentz-Fit
+# (Skript-Gleichung 1.10) aufnehmen -> Parameter Dämpfungskonstante y bestimmen
+# ACHTUNG: nur Peaks auswählen, deren Frequenzen größer sind als die Peaks der Transferfunktion, siehe 0_Transferfunktion.py
+# y und 1/y über Resonanzordnung plotten
